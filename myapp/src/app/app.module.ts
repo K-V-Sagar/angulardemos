e01ec80094d873aa31d2ebf4d3409663416196ae
K-V-Sagar <sgr.venkat24@gmail.com>
2022-09-17 05:38:41 +0530

    update ==> 17/9/22

---
+++ myapp/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductComponent } from './products/product/product.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ProductsComponent,
+    ProductComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
