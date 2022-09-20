5e9d8bc9d6b799d8a502109ab46425412176bff2
K-V-Sagar <sgr.venkat24@gmail.com>
2022-09-20 17:09:51 +0530

    update ==> 20/9/22

--- myapp/src/app/products/product/product.component.ts
+++ myapp/src/app/products/product/product.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
+  productId = 10;
+  productStatus = 'InStock';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  getColor() {
+    return this.productStatus === 'InStock' ? 'green' : 'red';
   }
-
 }
