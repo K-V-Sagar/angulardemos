e505a7f86a96e2c9b47ee71cb0372469bb2b7124
K-V-Sagar <sgr.venkat24@gmail.com>
2022-09-21 13:17:46 +0530

    changes

--- myapp/src/app/server/server.component.ts
+++ myapp/src/app/server/server.component.ts
@@ -1,28 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+  styleUrls: ['./server.component.css'],
 })
 export class ServerComponent implements OnInit {
-
   serverId: number = 10;
   serverStatus: string = 'online';
-  serverCreationStatus='No Server Created';
-  allowNewServer=true;
+  serverCreationStatus = 'No Server Created';
+  allowNewServer = true;
 
-  constructor() { }
+  constructor(private router: Router, private currentRoute: ActivatedRoute) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  getServerStatus() {
+    return this.serverStatus;
   }
 
-  getServerStatus(){
-    return 'ss: ' +this.serverStatus;
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created';
   }
 
-  onCreateServer(){
-    this.serverCreationStatus='Server was created!';
+  gotoAccounts() {
+    this.router.navigate(['accounts'], { relativeTo: this.currentRoute });
   }
-
 }
